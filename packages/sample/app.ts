3211266c9bf36b91bb60316dd67d3fa42021f516
Luiz Zheng <zl910627@yahoo.com>
2023-01-12 11:50:53 +0700

    just lint

--- packages/sample/app.ts
+++ packages/sample/app.ts
@@ -1,44 +1,44 @@
-import {AddressInfo} from 'net'
+import dotenv from 'dotenv'
+import { AddressInfo } from 'net'
 import express from 'express'
 import _ from 'lodash'
-import dotenv from 'dotenv'
 import Me3 from '@me3/keysmith'
 
 dotenv.config()
 
 const app = express()
 const server = app.listen(process.env.PORT || 5000, () => {
-  const {port} = server.address() as AddressInfo
-  console.log('server is running on port', port)
+    const { port } = server.address() as AddressInfo
+    console.log('server is running on port', port)
 })
 
 const me3 = new Me3({
-  endpoint: process.env.endpoint,
-  partnerId: process.env.partner,
-  client_id: process.env.google,
-  client_secret: process.env.secret,
-  redirect_uris: [process.env.redirect],
+    endpoint: process.env.endpoint,
+    partnerId: process.env.partner,
+    client_id: process.env.google,
+    client_secret: process.env.secret,
+    redirect_uris: [process.env.redirect],
 })
 
 app.get('/', async function (req, res) {
-  const code = _.get(req.query, 'code') as string
-  if (_.isEmpty(code)) {
-    const auth_url = me3.getGAuthUrl()
-    res.redirect(auth_url)
-    return
-  }
-
-  const success = await me3.getGToken(code)
-  if (success) {
-    try {
-      const wallets = await me3.getWallets()
-      console.table(wallets)
-      res.redirect('https://www.avarta.io/')
-    } catch (e) {
-      res.json({error: 'Oops, ERROR!', msg: e.message})
+    const code = _.get(req.query, 'code') as string
+    if (_.isEmpty(code)) {
+        const auth_url = me3.getGAuthUrl()
+        res.redirect(auth_url)
+        return
     }
-    return
-  }
 
-  res.json({error: 'Can\'t recover the wallets'})
+    const success = await me3.getGToken(code)
+    if (success) {
+        try {
+            const wallets = await me3.getWallets()
+            console.table(wallets)
+            res.redirect('https://www.avarta.io/')
+        } catch (e) {
+            res.json({ error: 'Oops, ERROR!', msg: e.message })
+        }
+        return
+    }
+
+    res.json({ error: 'Can\'t recover the wallets' })
 })
