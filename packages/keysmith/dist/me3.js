87aae3c2873bb31b16f76d2ac7454bc504add061
Luiz Zheng <zl910627@yahoo.com>
2023-01-12 09:13:17 +0700

    fix minor issue

--- packages/keysmith/dist/me3.js
+++ packages/keysmith/dist/me3.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -43,7 +66,7 @@ var path_1 = __importDefault(require("path"));
 var lodash_1 = __importDefault(require("lodash"));
 var qr_with_logo_1 = __importDefault(require("qr-with-logo"));
 var randomstring_1 = __importDefault(require("randomstring"));
-var bip39_1 = __importDefault(require("bip39"));
+var bip39 = __importStar(require("bip39"));
 var axios_1 = __importDefault(require("axios"));
 var types_1 = require("./types");
 var wallet_1 = __importDefault(require("./wallet"));
@@ -221,7 +244,7 @@ var Me3 = (function () {
                             result[lodash_1["default"].toLower(acc.series)] = list;
                             return result;
                         }, {});
-                        mnemonic = bip39_1["default"].generateMnemonic();
+                        mnemonic = bip39.generateMnemonic();
                         wallets = new Array();
                         _loop_1 = function (key, list) {
                             var wallet;
