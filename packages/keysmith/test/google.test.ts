9109b76b89c05fe58aeda36d3338eaf06307cd80
Luiz Zheng <zl910627@yahoo.com>
2023-03-10 13:50:53 +0700

    Unit test fixed

--- packages/keysmith/test/google.test.ts
+++ packages/keysmith/test/google.test.ts
@@ -3,7 +3,7 @@ import chaiAsPromised from 'chai-as-promised'
 import { describe } from 'mocha'
 
 import Google from '../src/google'
-import { ALICE, REDIRECTED, CONFIG } from './fixtures/configs'
+import { ALICE } from './fixtures/configs'
 
 chai.use(chaiAsPromised)
 const { expect } = chai
@@ -16,39 +16,12 @@ describe.skip('Google OAuth Client Object testing', () => {
   let gClient: Google
 
   before(() => {
-    gClient = new Google(
-      CONFIG.client_id,
-      CONFIG.client_secret,
-      CONFIG.redirect_uris
-    )
-  })
-
-  it('Google.generateAuthUrl() - should be able to return an oauth2 url', async () => {
-    const urlStartsWithProperUrl = gClient.generateAuthUrl().startsWith('https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope')
-
-    expect(urlStartsWithProperUrl).to.be.ok
-  })
-
-  it('Google::getTokens', async function () {
-    await gClient.getTokens(REDIRECTED)
-    const email = await gClient.getUserEmail()
-    expect(email).to.be.ok
+    gClient = new Google('')
   })
 
   it('Google::uploadFile & loadFile', async function () {
-    const email = await gClient.getUserEmail()
-    expect(email).to.be.ok
-
-    // const imgId = await gClient.saveFiles(
-    //   gClient.b642Readable(TEST_QR),
-    //   'hello.png',
-    //   'image/png'
-    // )
-    // console.log(imgId)
-    // expect(imgId).to.be.ok
-
     const secure = JSON.stringify(ALICE)
-    const jsonId = await gClient.saveFiles(
+    const jsonId = await gClient.saveFile(
       gClient.str2Readable(secure),
       'hello.json',
       'application/json'
